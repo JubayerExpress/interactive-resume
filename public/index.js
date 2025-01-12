7befaca7472f7ac2ea014fb8fe2f6b66de54c5f0
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-13 00:12:52 +0600

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Import CSS reset or other base styles (if needed)
+// import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
