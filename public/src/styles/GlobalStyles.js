ea91d838dadf8611f9cdaae1ae75a62e4379f2fe
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-13 00:10:39 +0600

    Update GlobalStyles.js

--- public/src/styles/GlobalStyles.js
+++ public/src/styles/GlobalStyles.js
@@ -1,1 +1,47 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    background-color: #f5f5f5;
+    color: #333;
+    line-height: 1.6;
+  }
+
+  h1, h2, h3 {
+    color: #333;
+    margin-bottom: 1rem;
+  }
+
+  a {
+    color: #007bff;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 2rem;
+  }
+
+  .section {
+    padding: 4rem 0;
+  }
+
+  .center {
+    text-align: center;
+  }
+`;
+
+export default GlobalStyle;
 
