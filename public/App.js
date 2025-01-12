ac1cae7474b811dadfbd588367320ec791cd2158
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-13 00:11:48 +0600

    Update App.js

--- public/App.js
+++ public/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Experience from './components/Experience';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import GlobalStyle from './styles/GlobalStyles';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <About />
+      <Experience />
+      <Skills />
+      <Contact />
 
