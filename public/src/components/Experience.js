95e6b030cdc9883b54fae490cf81833f511abb58
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-13 23:15:09 +0600

    Update Experience.js

--- public/src/components/Experience.js
+++ public/src/components/Experience.js
@@ -1,49 +1,20 @@
 import React, { useEffect } from 'react';
-import { gsap } from 'gsap';
-import styled from 'styled-components';
-
-const ExperienceSection = styled.section`
-  padding: 4rem 0;
-`;
-
-const ExperienceList = styled.ul`
-  list-style: none;
-`;
-
-const ExperienceItem = styled.li`
-  margin-bottom: 2rem;
-  h3 {
-    font-size: 1.5rem;
-  }
-  p {
-    font-size: 1.2rem;
-    color: #666;
-  }
-`;
+import gsap from 'gsap';
 
 const Experience = () => {
   useEffect(() => {
-    gsap.from('.experience-item', { duration: 1, y: 50, opacity: 0, stagger: 0.3 });
+    gsap.from('.experience', { duration: 1, x: -200, opacity: 0, delay: 1 });
   }, []);
 
   return (
-    <ExperienceSection id="experience">
-      <div className="container">
-        <h2>Experience</h2>
-        <ExperienceList>
-          <ExperienceItem className="experience-item">
-            <h3>Frontend Developer - XYZ Corp</h3>
-            <p>January 2020 - Present</p>
-          </ExperienceItem>
-          <ExperienceItem className="experience-item">
-            <h3>Junior Web Developer - ABC Tech</h3>
-            <p>June 2018 - December 2019</p>
-          </ExperienceItem>
-        </ExperienceList>
-      </div>
-    </ExperienceSection>
+    <section className="experience">
+      <h2>Experience</h2>
+      <ul>
+        <li>Software Developer at XYZ - 2020 to Present</li>
+        <li>Front-end Developer at ABC - 2018 to 2020</li>
+      </ul>
+    </section>
   );
 };
 
 export default Experience;
-
