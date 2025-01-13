cbf586998c48bc04b7c8fcf8bbdf971b99113b05
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-13 23:14:21 +0600

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-// Import CSS reset or other base styles (if needed)
-// import './index.css';
+import GlobalStyles from './styles/GlobalStyles';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
-
