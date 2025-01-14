b6c147cd9f893ca8014c9755ad99b58a90e5b67d
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-01-15 00:20:38 +0600

    Update App.js

--- public/App.js
+++ public/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 import About from './components/About';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+import { gsap } from 'gsap';
 
-const App = () => {
+function App() {
+  useEffect(() => {
+    gsap.from('h1', { duration: 1, y: -100, opacity: 0 });
+    gsap.from('p', { duration: 1, x: -100, opacity: 0, delay: 0.5 });
+    gsap.from('h2', { duration: 1, opacity: 0, delay: 1 });
+  }, []);
+
   return (
-    <div>
+    <>
+      <GlobalStyles />
       <Header />
       <About />
       <Experience />
       <Skills />
       <Contact />
-    </div>
+    </>
   );
-};
+}
 
 export default App;
